8d472b240d598b93f91763495e2f62a5d0156888
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-24 19:48:04 +0300

    Create Filter.jsx

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { setFilter } from 'store/filterSlice';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const changeFilter = e => {
+    dispatch(setFilter(e.currentTarget.value));
+  };
+
+  return (
+    <label>
+      Find contact by name <br />
+      <input type="text" onChange={changeFilter}></input>
+    </label>
+  );
+};
