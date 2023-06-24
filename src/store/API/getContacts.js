67e5de7e3c564b8eec320b227af712de908be7c0
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-24 19:49:10 +0300

    Create getContacts.js

---
+++ src/store/API/getContacts.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const getContacts = async () => {
+  const { data } = await axios(
+    'https://connections-api.herokuapp.com/contacts'
+  );
+  return data;
+};
+
