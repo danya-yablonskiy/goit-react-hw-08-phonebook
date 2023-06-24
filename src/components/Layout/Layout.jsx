739b1f2ff504a262d2b98727a6b902b8b2c8034e
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-24 19:48:36 +0300

    Create Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import AppBar from 'components/AppBar/AppBar';
+
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <AppBar />
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
