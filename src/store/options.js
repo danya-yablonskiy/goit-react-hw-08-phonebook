f58f518f6dbe3c3a88727f8c65cbed129d7616c4
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-24 19:49:28 +0300

    Create options.js

---
+++ src/store/options.js
@@ -0,0 +1,73 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getContacts } from './API/getContacts';
+import { postContacts } from './API/postContacts';
+import { deleteContacts } from './API/deleteContacts';
+import { registerUser } from './API/registerUser';
+import axios from 'axios';
+import { loginUser } from './API/loginUser';
+import { logOutUser } from './API/logOutUser';
+import { refreshUser } from './API/refreshUser';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const getContactsThunk = createAsyncThunk(
+  'contacts/getContacts',
+  async () => {
+    return await getContacts();
+  }
+);
+
+export const postContactsThunk = createAsyncThunk(
+  'contacts/postContacts',
+  async newContact => {
+    return await postContacts(newContact);
+  }
+);
+
+export const deleteContactsThunk = createAsyncThunk(
+  'contacts/deleteContacts',
+  async idContacts => {
+    return await deleteContacts(idContacts);
+  }
+);
+
+export const registerUserThunk = createAsyncThunk(
+  'auth/register',
+  async newUser => {
+    const res = await registerUser(newUser);
+
+    setAuthHeader(res.token);
+    return res;
+  }
+);
+
+export const loginUserThunk = createAsyncThunk('auth/login', async user => {
+  const res = await loginUser(user);
+  setAuthHeader(res.token);
+  return res;
+});
+
+export const logOutUserThunk = createAsyncThunk('auth/logout', async () => {
+  await logOutUser();
+  clearAuthHeader();
+});
+
+export const refreshUserThunk = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const { token } = thunkAPI.getState().auth;
+
+    if (!token) {
+      return thunkAPI.rejectWithValue('No valid token');
+    }
+    setAuthHeader(token);
+    const res = await refreshUser();
+    return res;
+  }
+);
