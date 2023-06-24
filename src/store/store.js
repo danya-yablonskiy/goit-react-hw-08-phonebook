35e36fa798c5fa0702fefeaf3847df105c4ba239
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-24 19:49:32 +0300

    Create store.js

---
+++ src/store/store.js
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './authSlice';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistConfig, authReducer),
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
+export const persistor = persistStore(store);
