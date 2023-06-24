e1d64e96eb31a1ca58ef57ff736e9b7c3a10e674
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-06-24 19:49:16 +0300

    Create postContacts.js

---
+++ src/store/API/postContacts.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const postContacts = async newContact => {
+  const { data } = await axios.post(
+    'https://connections-api.herokuapp.com/contacts',
+    newContact
+  );
+
+  return data;
+};
